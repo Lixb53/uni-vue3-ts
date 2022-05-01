ba4ee4e887eba308dadd8da881b3a5eb74132aa7
Lixb53 <li946226@sina.com>
2022-05-01 17:27:04 +0800

    添加 colorui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+
+import './styles/icon.css';
+import './styles/main.css';
+
 import { setupStore } from '/@/store';
 export function createApp() {
   const app = createSSRApp(App);
